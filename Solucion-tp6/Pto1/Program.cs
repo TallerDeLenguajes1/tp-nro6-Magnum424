cce72872e2bb24271a9e7ad91cae35322547fe97
Alejandro <ale_arc2015@hotmail.com>
2019-05-09 20:03:36 -0300

    Tp6

---
+++ Solucion-tp6/Pto1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, inverso, rem;
+            inverso = 0;
+            a = int.Parse(Console.ReadLine());
+            if (a >= 0)
+            {
+                while (a != 0)
+                {
+                    rem = a % 10;
+                    inverso = inverso * 10 + rem;
+                    a = a / 10;
+                }
+                Console.WriteLine("Número invertido:" + inverso);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Pos no");
+            }
+        }
+    }
+}
---
+++ Solucion-tp6/Pto2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pto2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, aux;
+            string op;
+            Console.WriteLine("1.Suma");
+            Console.WriteLine("2.Resta");
+            Console.WriteLine("3.Producto");
+            Console.WriteLine("4.Division");
+            Console.WriteLine("Ingrese la operacion a realizar:");
+            op = Console.ReadLine();
+            Console.WriteLine("Ingrese los numeros a operar:");
+            num1 = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());
+            switch (op)
+            {
+                case "Suma":
+                    aux = num1 + num2;
+                    Console.WriteLine("La suma es:" + aux);
+                    Console.ReadKey();
+                    break;
+                case "Resta":
+                    aux = num1 - num2;
+                    Console.WriteLine("La resta es:" + aux);
+                    Console.ReadKey();
+                    break;
+                case "Producto":
+                    aux = num1 * num2;
+                    Console.WriteLine("El producto es:" + aux);
+                    Console.ReadKey();
+                    break;
+                case "Division":
+                    aux = num1 / num2;
+                    Console.WriteLine("La division es:" + aux);
+                    Console.ReadKey();
+                    break;
+                default:
+                    Console.WriteLine("NO");
+                    break;
+            }
+        }
+    }
+}
