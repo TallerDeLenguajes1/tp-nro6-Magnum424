1930e48884f649038904938f3d921e61a7702c98
Alejandro <ale_arc2015@hotmail.com>
2019-05-27 22:30:36 -0300

    Done

--- Solucion-tp6/Pto5/Program.cs
+++ Solucion-tp6/Pto5/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Pto5
@@ -10,6 +11,18 @@ namespace Pto5
     {
         static void Main(string[] args)
         {
+            string contenido;
+            string Condicion_1 = @"\A[w]{3}(\.){a-z0-9}+(\.)(com|net|info|org)\Z";//Expresión regular: dirección web
+            string Condicion_2 = @"\A(\w+\.?\w*\@\w+\.)(com)\Z";//Expresión regular: correo electrónico
+            contenido = Console.ReadLine();
+            if (Regex.IsMatch(contenido, Condicion_1))
+            {
+                Console.WriteLine("Escribio una dirección web correcta.");
+            }
+            if (Regex.IsMatch(contenido, Condicion_2))
+            {
+                Console.WriteLine("Escribió una dirección de e-mail correcta.");
+            }
         }
     }
 }
